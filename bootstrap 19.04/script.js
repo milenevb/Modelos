f6cf8ea67e83ddb2fbbbc458529f1bbb328617d4
AnaaJulia <milene.batista.estudante@eteot.rio.br>
2022-04-26 09:59:57 -0300

    incompleto

--- bootstrap 19.04/script.js
+++ bootstrap 19.04/script.js
@@ -1,6 +1,7 @@
 function maiusculo() {
-    form.nome.value = form.nome.value.toUpperCase()  //nome do formulario. name. value
+    form.nome.value = form.nome.value.toUpperCase()  //name do formulario. name. value
 }
+
 function calculo(){
     //entrada
     let horario = document.form.horario.value;
@@ -19,3 +20,45 @@ function calculo(){
     //saida
     document.getElementById("total").value = valor;
 }
+function adiciona() {
+    //entrada
+    let guloseima = document.form.guloseima.value;
+
+    //processamemto
+    if (guloseima == "guloseimas"){
+        Number (valor) += 350
+    }
+    else if ( guloseima == "personagens"){
+        Number (valor) += 250
+    }
+    else if (guloseima == "atracoes" ){
+        Number (valor) += 300
+    }
+    else if (guloseima == "maquiagem"){
+        Number (valor) += 150
+    }
+    else if (guloseima == "brindes"){
+        Number (valor) += 100
+    }
+
+    // saída
+
+    apagar = document.getElementById("total").value;
+}
+
+
+/*
+    function confirmar(form){
+        
+        apagar = document.getElementById('total').value;
+        alert (apagar);
+        if (form.nome.value == ""){
+            alert("O Campo Nome é obrigatório.")
+            form.nome.focus();
+            return false;
+        }
+        else {
+            alert("enviou!")
+        }
+    }
+*/
